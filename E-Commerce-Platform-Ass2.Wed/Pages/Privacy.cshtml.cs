99eae80fa4aeb6ddcd334e70e7e600416d6cb974
Đặng Lê Anh Tuấn <dangleanhtuanpaula@gmail.com>
2026-01-26 08:35:50 +0700

    Add project files.

---
+++ E-Commerce-Platform-Ass2.Wed/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Commerce_Platform_Ass2.Wed.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
