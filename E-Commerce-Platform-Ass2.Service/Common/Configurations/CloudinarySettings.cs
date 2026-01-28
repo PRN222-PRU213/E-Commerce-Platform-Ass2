a0d9af41ab87a3615d37e2085eeb2d2b583261a2
Đặng Lê Anh Tuấn <dangleanhtuanpaula@gmail.com>
2026-01-28 11:01:05 +0700

    Add CloudinarySettings class for image upload configuration. Define properties for CloudName, ApiKey, ApiSecret, and UploadPreset to facilitate integration with Cloudinary in the E-Commerce platform.

---
+++ E-Commerce-Platform-Ass2.Service/Common/Configurations/CloudinarySettings.cs
@@ -0,0 +1,10 @@
+namespace E_Commerce_Platform_Ass2.Service.Common.Configurations
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; } = string.Empty;
+        public string ApiKey { get; set; } = string.Empty;
+        public string ApiSecret { get; set; } = string.Empty;
+        public string UploadPreset { get; set; } = string.Empty;
+    }
+}
